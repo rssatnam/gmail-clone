8af17e8947f516d90a008bc09520474009b415ff
Satnam Singh <rssatnam@gmail.com>
2021-06-19 09:14:36 +0530

    Header and Sidebar Components Styled

---
+++ src/Header.js
@@ -0,0 +1,42 @@
+import React from "react";
+import "./Header.css";
+import MenuIcon from "@material-ui/icons/Menu";
+import { Avatar, IconButton } from "@material-ui/core";
+import SearchIcon from "@material-ui/icons/Search";
+import ArrowDropDownIcon from "@material-ui/icons/ArrowDropDown";
+import AppsIcon from "@material-ui/icons/Apps";
+import NotificationsIcon from "@material-ui/icons/Notifications";
+
+function Header() {
+  return (
+    <div className="header">
+      <div className="header__left">
+        <IconButton>
+          <MenuIcon />
+        </IconButton>
+        <img
+          src="https://www.myce.com/wp-content/images_posts/2017/05/5847fafdcef1014c0b5e48ce.png"
+          alt=""
+        />
+      </div>
+
+      <div className="header__middle">
+        <SearchIcon />
+        <input type="text" placeholder="Search mail" />
+        <ArrowDropDownIcon className="header__inputCaret" />
+      </div>
+
+      <div className="header__right">
+        <IconButton>
+          <AppsIcon />
+        </IconButton>
+        <IconButton>
+          <NotificationsIcon />
+        </IconButton>
+        <Avatar />
+      </div>
+    </div>
+  );
+}
+
+export default Header;
