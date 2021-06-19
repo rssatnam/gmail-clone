5f577b270d9c7a8045ecdaa371cd9e3931d846f1
Satnam Singh <rssatnam@gmail.com>
2021-06-19 12:03:53 +0530

    middle/body section starting

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,9 +1,18 @@
-import { Button } from "@material-ui/core";
+import { Button, IconButton } from "@material-ui/core";
 import AddIcon from "@material-ui/icons/Add";
 import React from "react";
 import "./Sidebar.css";
 import InboxIcon from "@material-ui/icons/Inbox";
 import SidebarOption from "./SidebarOption";
+import StarIcon from "@material-ui/icons/Star";
+import AccessTimeIcon from "@material-ui/icons/AccessTime";
+import LabelImportantIcon from "@material-ui/icons/LabelImportant";
+import NearMeIcon from "@material-ui/icons/NearMe";
+import NoteIcon from "@material-ui/icons/Note";
+import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
+import PersonIcon from "@material-ui/icons/Person";
+import DuoIcon from "@material-ui/icons/Duo";
+import PhoneIcon from "@material-ui/icons/Phone";
 
 function Sidebar() {
   return (
@@ -15,7 +24,32 @@ function Sidebar() {
         Compose
       </Button>
 
-      <SidebarOption Icon={InboxIcon} title="Inbox" number={54} />
+      <SidebarOption
+        Icon={InboxIcon}
+        title="Inbox"
+        number={54}
+        selected={true}
+      />
+      <SidebarOption Icon={StarIcon} title="Starred" number={10} />
+      <SidebarOption Icon={AccessTimeIcon} title="Snoozed" number={10} />
+      <SidebarOption Icon={LabelImportantIcon} title="Important" number={10} />
+      <SidebarOption Icon={NearMeIcon} title="Sent" number={10} />
+      <SidebarOption Icon={NoteIcon} title="Drafts" number={10} />
+      <SidebarOption Icon={ExpandMoreIcon} title="More" number={10} />
+
+      <div className="sidebar__footer">
+        <div className="sidebar__footerIcons">
+          <IconButton>
+            <PersonIcon />
+          </IconButton>
+          <IconButton>
+            <DuoIcon />
+          </IconButton>
+          <IconButton>
+            <PhoneIcon />
+          </IconButton>
+        </div>
+      </div>
     </div>
   );
 }
