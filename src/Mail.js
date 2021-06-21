bd3aa4e6fcbc46fea09df9a6c9cae8891e6a9079
Satnam Singh <rssatnam@gmail.com>
2021-06-21 16:42:49 +0530

    Header & Sidebar Styling Done - Starting Email Body Component

---
+++ src/Mail.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Mail.css";
+
+function Mail() {
+  return (
+    <div className="mail">
+      <h2>Single Mail</h2>
+    </div>
+  );
+}
+
+export default Mail;
